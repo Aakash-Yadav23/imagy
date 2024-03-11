da653e9289a06941bee526d6123a13778e85b7af
developer+2-at-118321773388 <aakashdev24@gmail.com>
2024-03-12 05:10:34 +0530

    added-routes mongoose models database actions navbar

---
+++ src/app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Transformations = ({ params: { id } }: { params: { id: string } }) => {
+  return (
+    <div>Transformations</div>
+  )
+}
+
+export default Transformations
