913fc2b4e2ace401da5c8d01767cc2a79bd1ef4d
developer+2-at-118321773388 <aakashdev24@gmail.com>
2024-03-12 06:41:46 +0530

    added-clerk-webhooks

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: ['/', '/api/register', '/api/webhooks/clerk', '/api/webhooks/stripe']
+    publicRoutes: ['/', '/api/register', '/api/webhooks/clerk', '/api/webhooks']
 });
 
 export const config = {
