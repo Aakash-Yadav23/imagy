5e4cfef2fce742473305e2b3180759e6beaf80c1
developer+2-at-118321773388 <aakashdev24@gmail.com>
2024-03-14 23:15:34 +0530

    middlewre-changed

--- src/middleware.ts
+++ src/middleware.ts
@@ -57,5 +57,5 @@ export function middleware(request: NextRequest) {
 
 // Apply the middleware to all routes
 export const config = {
-    matcher: ['/sign-up','/signin', '/profile',],
+    matcher: ['/sign-up','/transformations','credits', '/sign-in', '/', '/profile',],
 };
